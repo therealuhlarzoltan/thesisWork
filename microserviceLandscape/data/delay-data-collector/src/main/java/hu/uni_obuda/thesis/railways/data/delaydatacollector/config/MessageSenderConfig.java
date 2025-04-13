19f817df2eab42d6b854a889ddd3f7943c81d7c1
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-13 11:32:26 +0200

    quick save

--- microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/config/MessageSenderConfig.java
+++
@@ -1,7 +0,0 @@
-package hu.uni_obuda.thesis.railways.data.delaydatacollector.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MessageSenderConfig {
-}
