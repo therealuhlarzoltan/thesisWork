d0fc412eb6eaa25e1de5d504d82aba7d48a4f62e
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-20 19:21:02 +0200

    redis custom serialization configured

--- microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/config/RedisCacheConfig.java
+++ microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/config/RedisCacheConfig.java
@@ -1,5 +1,7 @@
 package hu.uni_obuda.thesis.railways.data.delaydatacollector.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.uni_obuda.thesis.railways.data.weatherdatacollector.dto.WeatherInfo;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +10,29 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.*;
 
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         return RedisCacheManager.builder(connectionFactory).build();
     }
 
+    @Bean
+    public ReactiveRedisTemplate<String, WeatherInfo> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+        RedisSerializer<String> keySerializer = new StringRedisSerializer();
+        RedisSerializer<WeatherInfo> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, WeatherInfo.class);
+
+        RedisSerializationContext<String, WeatherInfo> context = RedisSerializationContext
+                .<String, WeatherInfo>newSerializationContext(keySerializer)
+                .value(valueSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(connectionFactory, context);
+    }
 }
