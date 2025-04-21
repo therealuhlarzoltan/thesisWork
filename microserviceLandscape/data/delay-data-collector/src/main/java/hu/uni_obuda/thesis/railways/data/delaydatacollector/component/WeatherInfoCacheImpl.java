2a5905405242a10f2d20817d4535ece93633c3ea
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-21 22:59:05 +0200

    custom cache implementation

--- microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/component/WeatherInfoCacheImpl.java
+++ microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/component/WeatherInfoCacheImpl.java
@@ -2,10 +2,12 @@ package hu.uni_obuda.thesis.railways.data.delaydatacollector.component;
 
 import hu.uni_obuda.thesis.railways.data.weatherdatacollector.dto.WeatherInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -14,18 +16,24 @@ public class WeatherInfoCacheImpl implements WeatherInfoCache {
 
     private final ReactiveRedisTemplate<String, WeatherInfo> redisTemplate;
 
+    @Value("${caching.weather.cache-duration:12}")
+    private Integer cacheDuration;
+
     @Override
     public Mono<Boolean> isCached(String stationName, LocalDateTime dateTime) {
-        return null;
+        return redisTemplate.hasKey(toKey(stationName, dateTime));
     }
 
     @Override
     public Mono<Void> cacheWeatherInfo(WeatherInfo weatherInfo) {
-        return null;
+        String key = toKey(weatherInfo);
+        return redisTemplate.opsForValue()
+                .set(key, weatherInfo, Duration.ofHours(cacheDuration))
+                .then();
     }
 
     @Override
     public Mono<WeatherInfo> retrieveWeatherInfo(String stationName, LocalDateTime dateTime) {
-        return null;
+        return redisTemplate.opsForValue().get(toKey(stationName, dateTime));
     }
 }
