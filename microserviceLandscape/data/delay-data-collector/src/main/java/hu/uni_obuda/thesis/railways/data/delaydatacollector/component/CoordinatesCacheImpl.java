179709089dc1b0765a8b9315bd1b201515fbef56
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-22 22:12:03 +0200

    station coordinates cache interface created

---
+++ microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/component/CoordinatesCacheImpl.java
@@ -0,0 +1,9 @@
+package hu.uni_obuda.thesis.railways.data.delaydatacollector.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CoordinatesCacheImpl implements CoordinatesCache {
+}
