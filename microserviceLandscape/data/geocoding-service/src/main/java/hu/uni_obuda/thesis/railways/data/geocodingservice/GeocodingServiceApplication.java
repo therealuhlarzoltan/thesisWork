e45eb2fa860e52f0d709b94b7405ccb96b785673
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-22 14:48:56 +0200

    geocoding service application created

---
+++ microserviceLandscape/data/geocoding-service/src/main/java/hu/uni_obuda/thesis/railways/data/geocodingservice/GeocodingServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.uni_obuda.thesis.railways.data.geocodingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocodingServiceApplication.class, args);
+    }
+
+}
---
+++ microserviceLandscape/data/geocoding-service/src/test/java/hu/uni_obuda/thesis/railways/data/geocodingservice/GeocodingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.uni_obuda.thesis.railways.data.geocodingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
