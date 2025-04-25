a1191f2a9e189f714e7fd9dd2e373df757f67347
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-04-25 22:31:21 +0200

    moved message processing delay to the appropriate bean

--- microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/config/MessageProcessorConfig.java
+++ microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/config/MessageProcessorConfig.java
@@ -7,27 +7,40 @@ import hu.uni_obuda.thesis.railways.data.raildatacollector.workers.MessageProces
 import hu.uni_obuda.thesis.railways.data.raildatacollector.workers.MessageProcessorImpl;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.workers.ResponseMessageSender;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
 import java.util.function.Consumer;
 
 @RequiredArgsConstructor
 @Configuration
 public class MessageProcessorConfig {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MessageProcessorConfig.class);
+
     private final ObjectMapper objectMapper;
     private final RailDataCollector railDataCollector;
     private final ResponseMessageSender responseSender;
+    private MessageProcessor messageProcessor;
 
     @Value("${app.threadPoolSize:10}")
     Integer threadPoolSize;
     @Value("${app.taskQueueSize:100}")
     Integer taskQueueSize;
+    @Value("${railway.api.rate.limit.delay-between-requests:1000}")
+    Integer delayBetweenRequests;
+    @Value("${railway.api.rate.limit.number-of-concurrent-calls:3}")
+    Integer numberOfConcurrentCalls;
 
     @Bean(name = "messageProcessingScheduler")
     public Scheduler messageProcessingScheduler() {
@@ -35,7 +48,24 @@ public class MessageProcessorConfig {
     }
 
     @Bean
-    public Consumer<Message<Event<?, ?>>> messageProcessor() {
-        return new MessageProcessorImpl(objectMapper, railDataCollector, responseSender, messageProcessingScheduler());
+    public Consumer<Flux<Message<Event<?, ?>>>> messageProcessor() {
+        if (messageProcessor == null) {
+            this.messageProcessor = new MessageProcessorImpl(objectMapper, railDataCollector, responseSender, messageProcessingScheduler());
+        }
+        return flux -> flux
+                .publishOn(messageProcessingScheduler())
+                .delayElements(Duration.ofMillis(delayBetweenRequests))
+                .flatMap(this::processSingleMessage, numberOfConcurrentCalls)
+                .onErrorContinue((error, obj) -> {
+                    LOG.error("Error during processing: {}", error.getMessage(), error);
+                })
+                .then()
+                .subscribe();
+    }
+
+    private Mono<Void> processSingleMessage(Message<Event<?, ?>> eventMessage) {
+        return Mono.fromRunnable(() -> messageProcessor.accept(eventMessage))
+                .subscribeOn(messageProcessingScheduler())
+                .then();
     }
 }
