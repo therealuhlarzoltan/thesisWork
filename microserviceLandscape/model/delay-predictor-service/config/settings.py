bf512ed04a60039b65019e7e93d66c05c12011ee
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-05-11 20:10:17 +0200

    secret key replaced

--- microserviceLandscape/model/delay-predictor-service/config/settings.py
+++ microserviceLandscape/model/delay-predictor-service/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tfk3ju#ys5@%i07_rvt92uw%b+-gt^ob3b-hbbroy5=wq=o12q'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
