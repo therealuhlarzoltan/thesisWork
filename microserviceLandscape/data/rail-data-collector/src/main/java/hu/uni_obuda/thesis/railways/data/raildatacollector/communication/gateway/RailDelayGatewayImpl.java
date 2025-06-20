1356b1efa6b8626abb4a199c29b8560069911c7f
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-06-20 15:26:49 +0200

    rail data collector refactor

--- microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/communication/gateway/RailDelayGatewayImpl.java
+++ microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/communication/gateway/RailDelayGatewayImpl.java
@@ -7,8 +7,11 @@ import hu.uni_obuda.thesis.railways.data.raildatacollector.communication.respons
 import hu.uni_obuda.thesis.railways.util.exception.datacollectors.ApiException;
 import hu.uni_obuda.thesis.railways.util.exception.datacollectors.ExternalApiException;
 import hu.uni_obuda.thesis.railways.util.exception.datacollectors.InternalApiException;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
+import io.github.resilience4j.reactor.retry.RetryOperator;
+import io.github.resilience4j.retry.RetryRegistry;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,41 +30,48 @@ public class RailDelayGatewayImpl implements RailDelayGateway {
     private static final Logger LOG = LoggerFactory.getLogger(RailDelayGatewayImpl.class);
 
     private final RailDelayWebClient webClient;
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
+    private final RetryRegistry retryRegistry;
 
-    @CircuitBreaker(name = "getTimetableApi", fallbackMethod = "handleTimetableFallback")
-    @Retry(name = "getTimetableApi")
     @Override
     public Mono<ShortTimetableResponse> getShortTimetable(String from, String to, LocalDate date) {
         LOG.debug("Called short timetable gateway with parameters {}, {}, {}", from, to, date);
-        return webClient.getShortTimetable(from, to, date);
+        return webClient.getShortTimetable(from, to, date)
+                .transformDeferred(CircuitBreakerOperator.of(circuitBreakerRegistry.circuitBreaker("getTimetableApi")))
+                .transformDeferred(RetryOperator.of(retryRegistry.retry("getTimetableApi")))
+                .onErrorResume(this::handleFallback);
     }
 
-    @CircuitBreaker(name = "getTrainDetailsApi", fallbackMethod = "handleDetailsFallback")
-    @Retry(name = "getTrainDetailsApi")
     @Override
     public Mono<ShortTrainDetailsResponse> getShortTrainDetails(String trainUri) {
         LOG.debug("Called train details gateway with uri {}", trainUri);
-        return webClient.getShortTrainDetails(trainUri);
+        return webClient.getShortTrainDetails(trainUri)
+                .transformDeferred(CircuitBreakerOperator.of(circuitBreakerRegistry.circuitBreaker("getTrainDetailsApi")))
+                .transformDeferred(RetryOperator.of(retryRegistry.retry("getTrainDetailsApi")))
+                .onErrorResume(this::handleFallback);
     }
 
-    @CircuitBreaker(name = "getFullTimetableApi", fallbackMethod = "handleFullTimetableFallback")
-    @Retry(name = "getFullTimetableApi")
     @Override
     public Mono<TimetableResponse> getTimetable(String from, String to, LocalDate date) {
         LOG.debug("Called full timetable gateway with parameters {}, {}, {}", from, to, date);
-        return webClient.getTimetable(from, to, date);
+        return webClient.getTimetable(from, to, date)
+                .transformDeferred(CircuitBreakerOperator.of(circuitBreakerRegistry.circuitBreaker("getFullTimetableApi")))
+                .transformDeferred(RetryOperator.of(retryRegistry.retry("getFullTimetableApi")))
+                .onErrorResume(this::handleFallback);
     }
 
-    public Mono<ShortTimetableResponse> handleTimetableFallback(String from, String to, LocalDate date, Throwable throwable) throws MalformedURLException {
-        return Mono.error(resolveApiException(throwable));
-    }
-
-    public Mono<ShortTrainDetailsResponse> handleDetailsFallback(String trainUri, Throwable throwable) throws MalformedURLException {
-        return Mono.error(resolveApiException(throwable));
-    }
-
-    public Mono<TimetableResponse> handleFullTimetableFallback(String from, String to, LocalDate date, Throwable throwable) throws MalformedURLException {
-        return Mono.error(resolveApiException(throwable));
+    private <T> Mono<T> handleFallback(Throwable throwable) {
+        if (throwable instanceof CallNotPermittedException callNotPermittedException) {
+            LOG.error("Circuit breaker is open", callNotPermittedException);
+        }
+        ApiException apiException;
+        try {
+            apiException = resolveApiException(throwable);
+        } catch (MalformedURLException e) {
+            LOG.error("Encountered a Malformed URL while trying to resolve API Exception", e);
+            apiException = new InternalApiException("Encountered a Malformed URL while trying to resolve API Exception", null);
+        }
+        return Mono.error(apiException);
     }
 
     private ApiException resolveApiException(Throwable throwable) throws MalformedURLException {
