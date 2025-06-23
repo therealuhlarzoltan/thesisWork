9a2d3fc94b89e21e5220fc160c31ecd997cf797a
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-06-23 11:19:45 +0200

    service discovery enabled

--- microserviceLandscape/cloud/security-server/src/main/java/hu/uni_obuda/thesis/railways/cloud/securityserver/SecurityServerApplication.java
+++ microserviceLandscape/cloud/security-server/src/main/java/hu/uni_obuda/thesis/railways/cloud/securityserver/SecurityServerApplication.java
@@ -2,7 +2,9 @@ package hu.uni_obuda.thesis.railways.cloud.securityserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SecurityServerApplication {
 
