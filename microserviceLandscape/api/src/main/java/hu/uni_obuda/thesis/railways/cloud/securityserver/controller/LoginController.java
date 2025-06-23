aa055caf5fcb4eab20f5bc7598e01630270d6697
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-06-23 12:07:24 +0200

    login http method changed

--- microserviceLandscape/api/src/main/java/hu/uni_obuda/thesis/railways/cloud/securityserver/controller/LoginController.java
+++ microserviceLandscape/api/src/main/java/hu/uni_obuda/thesis/railways/cloud/securityserver/controller/LoginController.java
@@ -3,13 +3,10 @@ package hu.uni_obuda.thesis.railways.cloud.securityserver.controller;
 import hu.uni_obuda.thesis.railways.cloud.securityserver.dto.JwtResponse;
 import hu.uni_obuda.thesis.railways.cloud.securityserver.dto.LoginRequest;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 public interface LoginController {
-    @GetMapping("login")
+    @PostMapping("login")
     JwtResponse login(@Valid @RequestBody LoginRequest loginRequest);
     @DeleteMapping("logout")
     void logout(@RequestHeader("Authorization") String authHeader);
