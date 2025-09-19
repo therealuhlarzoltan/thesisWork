49254f546661acbdef57a9d9ebc17d53b1f870c3
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-09-20 01:32:32 +0200

    reverting changes in ElviraRailDataServiceImpl

--- microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/service/ElviraRailDataServiceImpl.java
+++ microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/service/ElviraRailDataServiceImpl.java
@@ -3,7 +3,9 @@ package hu.uni_obuda.thesis.railways.data.raildatacollector.service;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.communication.gateway.RailDelayGateway;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.communication.response.ElviraShortTimetableResponse;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.communication.response.ElviraShortTrainDetailsResponse;
-import hu.uni_obuda.thesis.railways.data.raildatacollector.components.TimetableCache;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.communication.response.ElviraTimetableResponse;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.components.ElviraTimetableCache;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.dto.TrainRouteResponse;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.dto.DelayInfo;
 import hu.uni_obuda.thesis.railways.util.exception.datacollectors.*;
 import io.netty.channel.ConnectTimeoutException;
@@ -34,6 +36,7 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Profile("data-source-elvira")
@@ -44,7 +47,7 @@ public class ElviraRailDataServiceImpl implements ElviraRailDataService {
     private static final Logger LOG = LoggerFactory.getLogger(ElviraRailDataServiceImpl.class);
 
     private final RailDelayGateway gateway;
-    private final TimetableCache timetableCache;
+    private final ElviraTimetableCache timetableCache;
 
     @Override
     public Flux<DelayInfo> getDelayInfo(String trainNumber, String from, String to, LocalDate date) {
@@ -83,6 +86,15 @@ public class ElviraRailDataServiceImpl implements ElviraRailDataService {
                 .flatMapMany(Flux::fromIterable);
     }
 
+    @Override
+    public Flux<TrainRouteResponse> planRoute(String from, String to, LocalDate date) {
+        return gateway.getTimetable(from, to, date)
+                .onErrorMap(WebClientResponseException.NotFound.class, this::mapNotFoundToExternalApiException)
+                .onErrorMap(WebClientResponseException.BadRequest.class, this::mapBadRequestToExternalApiException)
+                .onErrorMap(WebClientRequestException.class, this::mapWebClientRequestExceptionToApiException)
+                .flatMap(timetableResponse ->  mapToRouteResponse(timetableResponse, date))
+                .flatMapMany(Flux::fromIterable);
+    }
 
     private Mono<ElviraShortTimetableResponse.TimetableEntry> checkSchedule(Tuple4<LocalTime, LocalTime, String, ElviraShortTimetableResponse.TimetableEntry> schedule) {
         LocalTime now = LocalTime.now();
@@ -141,6 +153,121 @@ public class ElviraRailDataServiceImpl implements ElviraRailDataService {
                 .next()
                 .map(detail -> detail.getTrainInfo().getUrl())
                 .switchIfEmpty(Mono.error(new TrainNotInServiceException(trainNumber, date)));
+    }
+
+    private Mono<List<TrainRouteResponse>> mapToRouteResponse(ElviraTimetableResponse timetableResponse, LocalDate date) {
+        List<TrainRouteResponse> routes = new ArrayList<>();
+        for (var entry : timetableResponse.getTimetable()) {
+            if (entry.getTrainSegments().size() == 1) {
+                String scheduledDep = entry.getDetails().getFirst().getDep();
+                String scheduledArrival = entry.getTransferStations().getFirst().getScheduledArrival();
+                LocalDateTime scheduledDepartureTime = parseTimeSafe(scheduledDep).atDate(date);
+                LocalDateTime scheduledArrivalTime = parseTimeSafe(scheduledArrival).atDate(date);
+                String actualDep = entry.getDetails().getFirst().getDepReal();
+                String actualArrival = entry.getTransferStations().getFirst().getRealArrival();
+                LocalDateTime actualDepartureTime = null;
+                LocalDateTime actualArrivalTime = null;
+                if (actualDep != null && !actualDep.isBlank()) {
+                    actualDepartureTime = parseTimeSafe(actualDep).atDate(date);
+                    if (actualDepartureTime.isBefore(scheduledDepartureTime)) {
+                        Duration duration = Duration.between(actualDepartureTime, scheduledDepartureTime);
+                        if (duration.toMinutes() > 4 * 60) {
+                            actualDepartureTime = actualDepartureTime.plusDays(1);
+                        }
+                    }
+                }
+                if (actualArrival != null && !actualArrival.isBlank() && actualDep != null && !actualDep.isBlank()) {
+                    actualArrivalTime = parseTimeSafe(actualArrival).atDate(date);
+                    if (actualArrivalTime.isBefore(actualDepartureTime)) {
+                        actualArrivalTime = actualArrivalTime.plusDays(1);
+                    }
+                }
+                if (scheduledArrivalTime.isBefore(scheduledDepartureTime)) {
+                    scheduledArrivalTime = scheduledArrivalTime.plusDays(1);
+                }
+                var train = TrainRouteResponse.Train.builder()
+                        .trainNumber(entry.getTrainSegments().getFirst().getCode())
+                        .lineNumber(entry.getTrainSegments().getFirst().getVszCode())
+                        .fromStation(entry.getDetails().getFirst().getFrom())
+                        .fromTimeScheduled(scheduledDepartureTime.toString())
+                        .fromTimeActual(Objects.toString(actualDepartureTime, ""))
+                        .toStation(entry.getTransferStations().getFirst().getStationName())
+                        .toTimeScheduled(scheduledArrivalTime.toString())
+                        .toTimeActual(Objects.toString(actualArrivalTime, ""))
+                        .build();
+                var route = new TrainRouteResponse(List.of(train));
+                routes.add(route);
+            } else {
+                List<TrainRouteResponse.Train> trains = new ArrayList<>();
+                int scheduledDepartureTimeRolloverIndex = findRolloverIndexForRoutes(entry.getDetails(), LocalTime.parse(entry.getDetails().getFirst().getDep()), ElviraTimetableResponse.JourneyElement::getDep);
+                int scheduledArrivalRolloverIndex = findRolloverIndexForRoutes(entry.getTransferStations(), LocalTime.parse(entry.getDetails().getFirst().getDep()), ElviraTimetableResponse.TransferStation::getScheduledArrival);
+                int actualDepartureTimeRolloverIndex = findRolloverIndexForRoutes(entry.getDetails(), LocalTime.parse(entry.getDetails().getFirst().getDep()), ElviraTimetableResponse.JourneyElement::getDep);
+                int actualArrivalTimeRolloverIndex = findRolloverIndexForRoutes(entry.getTransferStations(), LocalTime.parse(entry.getDetails().getFirst().getDep()), ElviraTimetableResponse.TransferStation::getRealArrival);
+                try {
+                    for (int i = 0; i < entry.getTrainSegments().size(); i++) {
+                        String scheduledDep = entry.getDetails().get(i * 2).getDep();
+                        String scheduledArrival = entry.getTransferStations().get(i).getScheduledArrival();
+                        LocalDateTime scheduledDepartureTime = parseTimeSafe(scheduledDep).atDate(date);
+                        LocalDateTime scheduledArrivalTime = parseTimeSafe(scheduledArrival).atDate(date);
+                        String actualDep = entry.getDetails().get(i * 2).getDepReal();
+                        String actualArrival = entry.getTransferStations().get(i).getRealArrival();
+                        LocalDateTime actualDepartureTime = null;
+                        LocalDateTime actualArrivalTime = null;
+                    /*
+                    if (actualDep != null && !actualDep.isBlank()) {
+                        actualDepartureTime = parseTimeSafe(actualDep).atDate(date);
+                        if (actualDepartureTime.isBefore(scheduledDepartureTime)) {
+                            Duration duration = Duration.between(actualDepartureTime, scheduledDepartureTime);
+                            if (duration.toMinutes() > 4 * 60) {
+                                actualDepartureTime = actualDepartureTime.plusDays(1);
+                            }
+                        }
+                    }
+                     */
+                        if (scheduledDep != null && !scheduledDep.isBlank()) {
+                            scheduledDepartureTime = parseTimeSafe(scheduledDep).atDate(date);
+                            if (scheduledDepartureTimeRolloverIndex != -1 && i >= scheduledDepartureTimeRolloverIndex) {
+                                scheduledDepartureTime = scheduledDepartureTime.plusDays(1);
+                            }
+                        }
+                        if (actualDep != null && !actualDep.isBlank()) {
+                            actualDepartureTime = parseTimeSafe(actualDep).atDate(date);
+                            if (actualDepartureTimeRolloverIndex != -1 && i >= actualDepartureTimeRolloverIndex) {
+                                actualDepartureTime = actualDepartureTime.plusDays(1);
+                            }
+                        }
+                        if (actualArrival != null && !actualArrival.isBlank()) {
+                            actualArrivalTime = parseTimeSafe(actualArrival).atDate(date);
+                            if (actualArrivalTimeRolloverIndex != -1 && i >= actualArrivalTimeRolloverIndex) {
+                                actualArrivalTime = actualArrivalTime.plusDays(1);
+                            }
+                        }
+                        if (scheduledArrival != null && !scheduledArrival.isBlank()) {
+                            scheduledArrivalTime = parseTimeSafe(scheduledArrival).atDate(date);
+                            if (scheduledArrivalRolloverIndex != -1 && i >= scheduledArrivalRolloverIndex) {
+                                scheduledArrivalTime = scheduledArrivalTime.plusDays(1);
+                            }
+                        }
+                        var train = TrainRouteResponse.Train.builder()
+                                .trainNumber(entry.getTrainSegments().get(i).getCode())
+                                .lineNumber(entry.getTrainSegments().get(i).getVszCode())
+                                .fromStation(entry.getDetails().get(i * 2).getFrom())
+                                .fromTimeScheduled(scheduledDepartureTime.toString())
+                                .fromTimeActual(Objects.toString(actualDepartureTime, ""))
+                                .toStation(entry.getTransferStations().get(i).getStationName())
+                                .toTimeScheduled(scheduledArrivalTime.toString())
+                                .toTimeActual(Objects.toString(actualArrivalTime, ""))
+                                .build();
+                        trains.add(train);
+                    }
+                } catch (Exception e) {
+                    LOG.error("An error occurred while processing route", e);
+                    continue;
+                }
+                routes.add(new TrainRouteResponse(trains));
+            }
+        }
+        return Mono.just(routes);
     }
 
     private Mono<List<DelayInfo>> mapToDelayInfo(ElviraShortTrainDetailsResponse response, String trainNumber, LocalDate date) {
