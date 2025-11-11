68d2866c0ea5c7d5f82201f44d6700b03cd3e64d
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-11-11 18:54:10 +0100

    scheduler services implemented

--- microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/config/SchedulerConfig.java
+++ microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/config/SchedulerConfig.java
@@ -1,9 +1,22 @@
 package hu.uni_obuda.thesis.railways.data.raildatacollector.config;
 
+import hu.uni_obuda.thesis.railways.data.raildatacollector.entity.ScheduledDateEntity;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.entity.ScheduledIntervalEntity;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.entity.ScheduledJobEntity;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.util.adapter.ReactiveDateRepositoryAdapter;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.util.adapter.ReactiveIntervalRepositoryAdapter;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.util.adapter.ReactiveJobRepositoryAdapter;
+import hu.uni_obuda.thesis.railways.util.scheduler.ReactiveCustomScheduler;
+import hu.uni_obuda.thesis.railways.util.scheduler.repository.ReactiveCompositeJobRepository;
+import hu.uni_obuda.thesis.railways.util.scheduler.repository.ReactiveCompositeJobRepositoryImpl;
+import hu.uni_obuda.thesis.railways.util.scheduler.repository.util.JobModifiedEventHandler;
 import hu.uni_obuda.thesis.railways.util.scheduler.scanner.ReactiveScheduledJobScanner;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.TaskScheduler;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -19,4 +32,37 @@ public class SchedulerConfig {
     public ReactiveScheduledJobScanner scheduledJobScanner(ApplicationContext applicationContext) {
         return new ReactiveScheduledJobScanner(applicationContext);
     }
+
+    @Bean
+    public JobModifiedEventHandler<ScheduledJobEntity, ScheduledIntervalEntity, ScheduledDateEntity> jobModifiedEventHandler(
+            @Lazy ReactiveDateRepositoryAdapter dateRepository,
+            @Lazy ReactiveIntervalRepositoryAdapter intervalRepository) {
+        return new JobModifiedEventHandler<>(intervalRepository, dateRepository);
+    }
+
+    @Bean
+    public ReactiveCompositeJobRepository<ScheduledJobEntity> compositeJobRepository(
+            @Lazy ReactiveJobRepositoryAdapter jobRepository,
+            @Lazy ReactiveDateRepositoryAdapter dateRepository,
+            @Lazy ReactiveIntervalRepositoryAdapter intervalRepository,
+            @Lazy JobModifiedEventHandler<ScheduledJobEntity, ScheduledIntervalEntity, ScheduledDateEntity> eventHandler,
+            @Qualifier("jobRepositoryScheduler") Scheduler scheduler) {
+        return new ReactiveCompositeJobRepositoryImpl<>(
+                jobRepository,
+                intervalRepository,
+                dateRepository,
+                scheduler,
+                eventHandler
+        );
+    }
+
+    @Bean
+    public ReactiveCustomScheduler customScheduler(
+            ReactiveScheduledJobScanner jobScanner,
+            @Lazy ReactiveCompositeJobRepository<ScheduledJobEntity> jobRepository,
+            TaskScheduler taskScheduler,
+            @Qualifier("jobRepositoryScheduler") Scheduler repositoryScheduler) {
+        return new ReactiveCustomScheduler(jobScanner, jobRepository, repositoryScheduler, taskScheduler);
+    }
+
 }
--- microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/service/scheduler/impl/ScheduledDateServiceImpl.java
+++ microserviceLandscape/data/rail-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/raildatacollector/service/scheduler/impl/ScheduledDateServiceImpl.java
@@ -2,8 +2,17 @@ package hu.uni_obuda.thesis.railways.data.raildatacollector.service.scheduler.im
 
 import hu.uni_obuda.thesis.railways.data.common.dto.ScheduledDateRequest;
 import hu.uni_obuda.thesis.railways.data.common.dto.ScheduledDateResponse;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.entity.ScheduledDateEntity;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.entity.ScheduledJobEntity;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.mapper.ScheduledDateMapper;
 import hu.uni_obuda.thesis.railways.data.raildatacollector.service.scheduler.ScheduledDateService;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.util.adapter.ReactiveDateRepositoryAdapter;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.util.adapter.ReactiveJobRepositoryAdapter;
+import hu.uni_obuda.thesis.railways.util.exception.datacollectors.EntityNotFoundException;
+import hu.uni_obuda.thesis.railways.util.exception.datacollectors.InvalidInputDataException;
+import hu.uni_obuda.thesis.railways.util.scheduler.event.ScheduledJobModifiedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,28 +21,93 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class ScheduledDateServiceImpl implements ScheduledDateService {
 
+    private final ReactiveDateRepositoryAdapter dateRepository;
+    private final ReactiveJobRepositoryAdapter jobRepository;
+    private final ApplicationEventPublisher eventPublisher;
+    private final ScheduledDateMapper mapper;
+
     @Override
     public Flux<ScheduledDateResponse> findAll() {
-        return null;
+        return dateRepository.findAll().map(mapper::entityToApi);
     }
 
     @Override
     public Mono<ScheduledDateResponse> find(int id) {
-        return null;
+        return dateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new EntityNotFoundException(id, ScheduledDateEntity.class)))
+                .map(mapper::entityToApi);
     }
 
     @Override
     public Mono<ScheduledDateResponse> create(ScheduledDateRequest date) {
-        return null;
+        return dateRepository.existsByJobIdAndCronExpression(date.getJobId(), date.getCronExpression())
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.error(new InvalidInputDataException("Scheduled date with cron expression for job already exists"));
+                    }
+
+                    return jobRepository.existsById(date.getJobId())
+                            .flatMap(jobExists -> {
+                                if (!jobExists) {
+                                    return Mono.error(new EntityNotFoundException(date.getJobId(), ScheduledJobEntity.class));
+                                }
+                                return dateRepository.save(mapper.apiToEntity(date));
+                            });
+                })
+                .flatMap(savedDate ->
+                        jobRepository.findById(savedDate.getJobId())
+                                .switchIfEmpty(Mono.error(new EntityNotFoundException(savedDate.getJobId(), ScheduledJobEntity.class)))
+                                .delayUntil(this::publishJobModified)
+                                .thenReturn(savedDate)
+                )
+                .map(mapper::entityToApi);
     }
 
     @Override
     public Mono<ScheduledDateResponse> update(int id, ScheduledDateRequest date) {
-        return null;
+        return dateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new EntityNotFoundException(id, ScheduledDateEntity.class)))
+                .map(dateEntity -> updateScheduledDateEntity(dateEntity, date))
+                .flatMap(dateEntity -> {
+                    return dateRepository.existsByJobIdAndCronExpression(dateEntity.getJobId(), dateEntity.getCronExpression())
+                            .flatMap(exists -> {
+                                if (exists) {
+                                    return Mono.error(new InvalidInputDataException("Scheduled date with cron expression for job already exists"));
+                                }
+
+                                return dateRepository.save(dateEntity);
+                            });
+                })
+                .flatMap(savedDate ->
+                        jobRepository.findById(savedDate.getJobId())
+                                .switchIfEmpty(Mono.error(new EntityNotFoundException(id, ScheduledJobEntity.class)))
+                                .delayUntil(this::publishJobModified)
+                                .thenReturn(savedDate)
+                )
+                .map(mapper::entityToApi);
     }
 
     @Override
     public Mono<Void> delete(int id) {
-        return null;
+        return dateRepository.findById(id)
+                .switchIfEmpty(Mono.error(new EntityNotFoundException(id, ScheduledDateEntity.class)))
+                .flatMap(intervalEntity ->
+                        dateRepository.delete(intervalEntity)
+                                .then(jobRepository.findById(intervalEntity.getJobId()))
+                                .switchIfEmpty(Mono.error(new EntityNotFoundException(intervalEntity.getJobId(), ScheduledJobEntity.class)))
+                )
+                .delayUntil(this::publishJobModified)
+                .then();
+    }
+
+    private static ScheduledDateEntity updateScheduledDateEntity(ScheduledDateEntity date, ScheduledDateRequest dateRequest) {
+        date.setCronExpression(dateRequest.getCronExpression());
+        return date;
+    }
+
+    private Mono<Void> publishJobModified(ScheduledJobEntity job) {
+        return Mono.fromRunnable(() ->
+                eventPublisher.publishEvent(new ScheduledJobModifiedEvent<>(this, job))
+        );
     }
 }
