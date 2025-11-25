1c74904822fe9b5d7b85837563339f6993d0e6db
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-11-25 03:26:57 +0100

    new cache configured

--- microserviceLandscape/route/route-planner-service/src/main/java/hu/uni_obuda/thesis/railways/route/routeplannerservice/config/CacheConfig.java
+++ microserviceLandscape/route/route-planner-service/src/main/java/hu/uni_obuda/thesis/railways/route/routeplannerservice/config/CacheConfig.java
@@ -3,6 +3,7 @@ package hu.uni_obuda.thesis.railways.route.routeplannerservice.config;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import hu.uni_obuda.thesis.railways.data.geocodingservice.dto.GeocodingResponse;
+import hu.uni_obuda.thesis.railways.data.raildatacollector.dto.TrainRouteResponse;
 import hu.uni_obuda.thesis.railways.data.weatherdatacollector.dto.WeatherInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,14 +14,20 @@ import java.time.Duration;
 @Configuration
 public class CacheConfig {
 
-    @Value("${caching.geocoding.cache-in-minutes:180}")
+    @Value("${caching.geocoding.cache-duration-in-minutes:180}")
     private int geocodingCacheDuration;
     @Value("${caching.geocoding.cache-size:100}")
     private int geocodingCacheSize;
-    @Value("${caching.weather.cache-in-minutes:5}")
+    @Value("${caching.weather.cache-duration-in-minutes:5}")
     private int weatherCacheDuration;
     @Value("${caching.weather.cache-size:100}")
     private int weatherCacheSize;
+    @Value("${caching.route.cache-duration-in-seconds:30}")
+    private int routeCacheDuration;
+    @Value("${caching.route.cache-size:100}")
+    private int routeCacheSize;
+
+
 
     @Bean
     public Cache<String, GeocodingResponse> geocodingCache() {
@@ -35,6 +42,15 @@ public class CacheConfig {
         return Caffeine.newBuilder()
                 .maximumSize(weatherCacheSize)
                 .expireAfterWrite(Duration.ofMinutes(weatherCacheDuration))
+                .build();
+    }
+
+
+    @Bean
+    public Cache<String, TrainRouteResponse> trainRouteCache() {
+        return Caffeine.newBuilder()
+                .maximumSize(routeCacheSize)
+                .expireAfterWrite(Duration.ofSeconds(routeCacheDuration))
                 .build();
     }
 }
