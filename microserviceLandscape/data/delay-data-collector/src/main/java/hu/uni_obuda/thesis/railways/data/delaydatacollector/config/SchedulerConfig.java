e9253c45e19e16c14145b32bff5f6bb45b48e0f4
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-11-26 17:56:08 +0100

    removing unnecessary env variables

--- microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/config/SchedulerConfig.java
+++ microserviceLandscape/data/delay-data-collector/src/main/java/hu/uni_obuda/thesis/railways/data/delaydatacollector/config/SchedulerConfig.java
@@ -11,9 +11,8 @@ import hu.uni_obuda.thesis.railways.util.scheduler.repository.ReactiveCompositeJ
 import hu.uni_obuda.thesis.railways.util.scheduler.repository.ReactiveCompositeJobRepositoryImpl;
 import hu.uni_obuda.thesis.railways.util.scheduler.repository.util.JobModifiedEventHandler;
 import hu.uni_obuda.thesis.railways.util.scheduler.scanner.ReactiveScheduledJobScanner;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,7 +42,7 @@ public class SchedulerConfig {
         return new JobModifiedEventHandler<>(intervalRepository, dateRepository);
     }
 
-    @ConditionalOnBean(Flyway.class)
+    @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", havingValue = "true", matchIfMissing = true)
     @DependsOn("flyway")
     @Bean
     public ReactiveCompositeJobRepository<ScheduledJobEntity> compositeJobRepository(
