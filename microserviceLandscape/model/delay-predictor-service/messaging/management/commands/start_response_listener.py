9f5156fee3b835edeaa26564da7b692ce756b06d
therealuhlarzoltan <therealuhlazoltan@gmail.com>
2025-12-04 18:20:39 +0100

    separating model training and not model training instances

--- microserviceLandscape/model/delay-predictor-service/messaging/management/commands/start_response_listener.py
+++ microserviceLandscape/model/delay-predictor-service/messaging/management/commands/start_response_listener.py
@@ -1,9 +1,17 @@
+import os
+
 from django.core.management.base import BaseCommand
 from messaging.consumer import start_consuming  # move your _consume_batch_responses logic here
+
+def is_training_instance() -> bool:
+    val = os.getenv("IS_TRAINING_INSTANCE", "true").strip().lower()
+    return val in ("1", "true", "yes", "y", "on")
+
 
 class Command(BaseCommand):
     help = 'Start RabbitMQ consumer for batch responses'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS("Starting RabbitMQ response listener..."))
-        start_consuming()
+        if is_training_instance():
+            self.stdout.write(self.style.SUCCESS("Starting RabbitMQ response listener..."))
+            start_consuming()
